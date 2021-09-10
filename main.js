519b939fec9dba68df8448e2f101b740664de18e
Chinmay Khopde <chinmaykhopde>
2021-09-10 17:59:32 +0530

    fixed image sizes

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-	ScrollReveal().reveal('.dark-overlay', {
-		duration: 2500,
-		delay: 1000
-	});
+ScrollReveal().reveal('.dark-overlay', {
+    duration: 2500,
+    delay: 1000
+});
 
-	ScrollReveal().reveal('div.right', {
-		origin: 'bottom',
-		interval: 2000,
-		delay: 3000,
-		useDelay: 'onload'
-	});
+ScrollReveal().reveal('div.right', {
+    origin: 'bottom',
+    interval: 2000,
+    delay: 3000,
+    useDelay: 'onload'
+});
 
-	ScrollReveal().reveal('div.left', {
-		origin: 'bottom',
-		interval: 2000,
-		delay: 4000,
-		useDelay: 'onload'
-	});
+ScrollReveal().reveal('div.left', {
+    origin: 'bottom',
+    interval: 2000,
+    delay: 4000,
+    useDelay: 'onload'
+});
 
-	ScrollReveal().reveal('h2', {
-		viewFactor: 0.2,
-		duration: 1500,
-		origin: 'left',
-		distance: '200px',
-		reset: true,
-	});
+ScrollReveal().reveal('h2', {
+    viewFactor: 0.2,
+    duration: 1500,
+    origin: 'left',
+    distance: '200px',
+    reset: true,
+});
 
-	ScrollReveal().reveal('.container p', {
-		viewFactor: 0.2,
-		duration: 1500,
-		origin: 'right',
-		distance: '200px',
-		reset: true,
-	});
+ScrollReveal().reveal('.container p', {
+    viewFactor: 0.2,
+    duration: 1500,
+    origin: 'right',
+    distance: '200px',
+    reset: true,
+});
 
-	ScrollReveal().reveal('i', {
-		duration: 8000,
-		viewFactor: 0.2,
-		rotate: {
-			x: 0,
-			y: 900,
-			z: 0
-		}
-	});
+ScrollReveal().reveal('i', {
+    duration: 8000,
+    viewFactor: 0.2,
+    rotate: {
+        x: 0,
+        y: 900,
+        z: 0
+    }
+});
